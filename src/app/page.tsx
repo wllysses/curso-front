c48194e92a1dd53956722cf4054ded517fced8d3
wllysses <wllyssestavares@gmail.com>
2026-07-20 13:59:09 -0300

    refactor: add sheet component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
 import Link from "next/link";
-import { CodeXmlIcon } from "lucide-react";
-import { buttonVariants } from "@/components/ui/button";
+import {
+  CodeXmlIcon,
+  BracesIcon,
+  FileTextIcon,
+  MenuIcon,
+  Braces,
+} from "lucide-react";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Classrooms } from "@/components/classrooms";
 import { Hero } from "@/components/hero";
 import Image from "next/image";
 
 import illustration from "@/../public/Gemini_Generated_Image_t3pa0tt3pa0tt3pa.png";
 import { Repositories } from "@/components/repositories";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function Home() {
   const links = [
     {
       label: "Material de Apoio",
       href: "#aulas",
+      icon: <FileTextIcon />,
     },
     {
       label: "Nossos Códigos",
       href: "#codigos",
+      icon: <BracesIcon />,
     },
     // {
     //   label: "Contatos",
@@ -26,28 +41,43 @@ export default function Home() {
 
   return (
     <>
-      <header className="border-b p-4">
+      <header className="border-b p-4 fixed top-0 left-0 w-full backdrop-blur-sm z-50">
         <div className="container mx-auto flex items-center justify-around flex-wrap max-sm:gap-6">
           <Link href="/" className="flex items-center gap-2">
             <div className="bg-primary p-2 rounded size-8 text-white flex items-center justify-center">
               <CodeXmlIcon />
             </div>
-            <h1 className="font-semibold">Paraibatec/Pronatec</h1>
+            <h1 className="font-semibold">Paraíbatec/Pronatec</h1>
           </Link>
 
-          <nav>
-            <ul className="flex items-center gap-4">
-              {links.map((link, index) => (
-                <Link
-                  href={link.href}
-                  key={index}
-                  className={buttonVariants({ variant: "secondary" })}
-                >
-                  {link.label}
-                </Link>
-              ))}
-            </ul>
-          </nav>
+          <Sheet>
+            <SheetTrigger
+              render={
+                <Button variant="outline" size="icon">
+                  <MenuIcon />
+                </Button>
+              }
+            />
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className={"font-bold"}>Menu</SheetTitle>
+              </SheetHeader>
+
+              <nav className="flex flex-col gap-4 p-4 w-full">
+                {links.map((link, index) => (
+                  <Link href={link.href} key={index} className="w-full">
+                    <Button
+                      className={"w-full justify-start"}
+                      variant="outline"
+                    >
+                      {link.icon}
+                      {link.label}
+                    </Button>
+                  </Link>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
 
           {/* <Button className="text-white font-semibold cursor-pointer">
             Login
